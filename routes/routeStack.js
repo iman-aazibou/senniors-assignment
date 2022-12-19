f7d00020ea545bbd42350cea65d561e610cf3b09
iman-aazibou <imanaazibou@hotmail.com>
2022-12-19 08:01:41 +0100

    animation

--- routes/routeStack.js
+++ routes/routeStack.js
@@ -1,13 +1,35 @@
-import { createStackNavigator } from "react-navigation-stack";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 
+//Views
 import Home from "../app/screens/Home";
 import CreateAccForm from "../app/screens/CreateAccForm";
 import LoggedUserHome from "../app/screens/LoggedUserHome";
 
+const config = {
+  animation: "spring",
+  config: {
+    stiffness: 1000,
+    damping: 500,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
+
+const transitionSpec = {
+  open: config,
+  close: config,
+};
+
 const screens = {
   Home: {
     screen: Home,
+    
   },
   Form: {
     screen: CreateAccForm,
@@ -17,5 +39,12 @@ const screens = {
   },
 };
 
-const routeStack = createStackNavigator(screens);
+const routeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    ...TransitionPresets.ModalSlideFromBottomIOS,
+    ...TransitionPresets.FadeFromBottomAndroid,
+    transitionSpec,
+  },
+});
+
 export default createAppContainer(routeStack);
